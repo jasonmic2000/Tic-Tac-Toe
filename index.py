58edd51441387675577054179241b9a27b1096e7
Jason Michael <jason_mic2000@yahoo.com>
2021-09-19 12:06:24 +0530

    Update index.py
    Added function for player choice input and replay

--- index.py
+++ index.py
@@ -72,3 +72,18 @@ def full_board_check(board):
             return False
     return True
 
+
+# Function to ask player's position choice
+def player_choice(board):
+    position = 0
+    
+    while position not in [1,2,3,4,5,6,7,8,9] or not space_check(board, position):
+        position = int(input('Choose your next position: (1-9) '))
+        
+    return position
+
+# Function to ask players if they want to play again
+def replay():
+    
+    return input('Do you want to play again? Enter Yes or No: ').lower().startswith('y')
+
